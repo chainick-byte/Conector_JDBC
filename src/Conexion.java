5b3d8f6a1592840808e07a4cb058c5e8f4e0289f
chainick-byte <igorrepyakh@gmail.com>
2021-01-15 16:23:13 +0000

    creo primer pul de conexiones

--- src/Conexion.java
+++ src/Conexion.java
@@ -4,29 +4,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author igorr
  */
 public class Conexion {
-    Connection conexion=null;
-    public int Conexion(){
-    String url="jdbc:mysql://192.168.182.129/discografica?serverTimeZone=UTC";
-    String user="root";
-    String password="";
-    
+
+    Connection conexion = null;
+
+    public int Conexion() {
+        BasicDataSource bdSource = new BasicDataSource();
+        bdSource.setUrl("jdbc:mysql://192.168.182.142/discografica?serverTimezone=UTC");
+        bdSource.setUsername("root");
+        bdSource.setPassword("");
+
         try {
-            conexion = DriverManager.getConnection(url,user,password);
-            if(conexion!=null){
+            conexion = bdSource.getConnection();
+            if (conexion != null) {
                 System.out.println("todo ok ");
-            }else{
+            } else {
                 System.out.println("todo mal");
             }
             return 1;
@@ -35,12 +38,13 @@ public class Conexion {
             return -1;
         }
     }
-    public void closeConexion(){
+
+    public void closeConexion() {
         try {
             conexion.close();
         } catch (SQLException ex) {
             Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }
